f37f428c62317060b2f5641ef2d00437459e20df
Mélissa Rissel <rissel.melissa@gmail.com>
2018-01-09 14:40:48 +0000

    edits to menu, responsive menu, favicon

--- header.php
+++ header.php
@@ -9,6 +9,13 @@
         <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
         <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
 
+        <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/img/favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/img/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/img/favicon/favicon-16x16.png">
+        <link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/img/favicon/manifest.json">
+        <link rel="mask-icon" href="<?php echo get_template_directory_uri(); ?>/img/favicon/safari-pinned-tab.svg" color="#00ffff">
+        <meta name="theme-color" content="#000000">
+
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="<?php bloginfo('description'); ?>">
@@ -34,3 +41,4 @@
         </nav>
 
           <a href="#" id="menu_button">Menu</a>
+          <a href="<?php echo get_home_url();?>" class="logo_link"></a>
--- social-bar.php
+++ social-bar.php
@@ -2,7 +2,7 @@
     <div id="social_bar">
 
         <ul>
-          <li id="social_tickets"><span class="tittle">Billeterie</span><a target="_blank" href="#"><i class="fa fa-ticket"></i> <span>Tickets</span></a></li>
+          <li id="social_tickets"><span class="tittle">Billeterie</span><a href="<?php echo get_home_url(); ?>/billetterie-en-ligne"><i class="fa fa-ticket"></i> <span>Tickets</span></a></li>
             <li id="social_facebook"><span class="tittle">Consulter&nbsp;notre&nbsp;page&nbsp;Facebook</span><a target="_blank" href="https://www.facebook.com/FestivalGNM/"><i class="fa fa-facebook"></i> <span>Facebook</span></a></li>
             <li id="social_youtube"><span class="tittle">Consulter&nbsp;notre&nbsp;page&nbsp;Youtube</span><a target="_blank" href="https://www.youtube.com/user/GrooveNMove1/"><i class="fa fa-youtube-play"></i> <span>Twitter</span></a></li>
             <li id="social_instagram"><span class="tittle">Consulter&nbsp;notre&nbsp;page&nbsp;Instagram</span><a target="_blank" href="https://www.instagram.com/groovenmove/"><i class="fa fa-instagram"></i> <span>Instagram</span></a></li>
